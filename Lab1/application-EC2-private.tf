d408a1a481e537c3d657bb338f96a83f41fdc679
hadeeryoussefahmed <hadeer.youssef.ahmed.ahmed@gmail.com>
2023-10-01 15:16:53 +0300

    commit after edit and remove kerpair private and 2 ec2 instances took the same name of tf key pair

--- Lab1/application-EC2-private.tf
+++ Lab1/application-EC2-private.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "application_ec2-private" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private-subnet-useast1b.id
   vpc_security_group_ids = [aws_security_group.sg2.id]
-  key_name = aws_key_pair.tf-key-pair-private.id
+  key_name = aws_key_pair.tf-key-pair.id
+
 
 
   tags = {
